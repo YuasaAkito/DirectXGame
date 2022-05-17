e68abd27412d02dff8891f8e45c460e31c973760
YuasaAkito <k021g1372@g.neec.ac.jp>
2022-05-17 13:51:55 +0900

    2-2

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,10 +1,15 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
@@ -12,9 +17,66 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	debugCamera_ = new DebugCamera(1600, 900); 
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+	worldTransform_.scale_ = { 5,5,5 };
+	Matrix4 matScale;
+	Matrix4 mat;
+	mat.m[0][0] = 1;
+	mat.m[1][1] = 1;
+	mat.m[2][2] = 1;
+	mat.m[3][3] = 1;
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1;
+	worldTransform_.rotation_ = { 3.14f / 4.0f,3.14f / 4.0f,0.0f };
+	Matrix4 matRotZ,matRotX,matRotY;
+	matRotZ.m[0][0] = cos(worldTransform_.rotation_.z);
+	matRotZ.m[0][1] = sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][0] = -sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][1] = cos(worldTransform_.rotation_.z);
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+	matRotX.m[1][1] = cos(worldTransform_.rotation_.x);
+	matRotX.m[1][2] = sin(worldTransform_.rotation_.x);
+	matRotX.m[2][1] = -sin(worldTransform_.rotation_.x);
+	matRotX.m[2][2] = cos(worldTransform_.rotation_.x);
+	matRotX.m[0][0] = 1;
+	matRotX.m[3][3] = 1;
+	matRotY.m[0][0] = cos(worldTransform_.rotation_.y);
+	matRotY.m[2][0] = sin(worldTransform_.rotation_.y);
+	matRotY.m[0][2] = -sin(worldTransform_.rotation_.y);
+	matRotY.m[2][2] = cos(worldTransform_.rotation_.y);
+	matRotY.m[1][1] = 1;
+	matRotY.m[3][3] = 1;
+	worldTransform_.translation_ = { 10,10,10 };
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+	matTrans.m[3][0] = worldTransform_.translation_.x;
+	matTrans.m[3][1] = worldTransform_.translation_.y;
+	matTrans.m[3][2] = worldTransform_.translation_.z;
+	matTrans.m[0][0] = 1;
+	matTrans.m[1][1] = 1;
+	matTrans.m[2][2] = 1;
+	matTrans.m[3][3] = 1;
+	worldTransform_.matWorld_ = mat;
+	worldTransform_.matWorld_ *= matScale;
+	worldTransform_.matWorld_ *= matRotZ;
+	worldTransform_.matWorld_ *= matRotX;
+	worldTransform_.matWorld_ *= matRotY;
+	worldTransform_.matWorld_ *= matTrans;
+	worldTransform_.TransferMatrix();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -37,12 +99,18 @@ void GameScene::Draw() {
 
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
+	for (int i = 0; i < 11; i++)
+	{
+		PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0 , i , 0), Vector3(10, i, 0), Vector4(255, 255, 255, 255));
+		PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(i, 0, 0), Vector3(i, 10, 0), Vector4(255, 255, 255, 255));
+	}
 	Model::PreDraw(commandList);
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
